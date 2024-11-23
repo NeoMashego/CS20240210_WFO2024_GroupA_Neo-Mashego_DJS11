8e520ab9b79ab9819e7d2235e453ee3c02294647
NeoMashego <mashegonoj@gmail.com>
2024-11-23 15:10:58 +0200

    setting Home page content

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+import {BrowserRouter, Routes, Route, Link, NavLink} from 'react-router-dom'
+import Home from './Pages/Home.jsx'
 import './App.css'
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+      <nav>
+        <Link to="/"> Home </Link>
+      </nav>
+      <Routes>
+        <Link path="/" element={< Home/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
