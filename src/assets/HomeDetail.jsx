6612338fefe4b18e1a575a3dc34c1d4ae45bf109
NeoMashego <mashegonoj@gmail.com>
2024-11-23 21:23:36 +0200

    fetched API, useParams and display detail of each Podcast

--- src/assets/HomeDetail.jsx
+++ src/assets/HomeDetail.jsx
@@ -1,7 +1,50 @@
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+
+const DataFetch = function Components(){
+    const [data, setData] = useState([])
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState("")
+    const {id} = useParams();       //retrieves URL path = id
+
+    useEffect( () =>{
+        fetch(`https://podcast-api.netlify.app/id/${id}`)
+        .then(response =>{
+            if(!response.ok){
+                throw new Error('ID details not found.')
+            } return response.json()
+        })
+        .then((data =>{
+            setData(data);
+            setLoading(false);
+        }))
+        .catch((error => {
+            setError('ID details not found.')
+            setLoading(false)
+        }))
+    })
+
+    if(loading){
+        return <div>Loading...</div>
+    }
+
+    if(error){
+        return <h2>{error}</h2>
+    }
+
+    //const SeasonsDisplay = 
+
+    return(
+        <div>
+            <h1>{data.title}</h1>
+            <p>{data.description}</p>
+        </div>
+    )
+}
 
 function HomeDetail(){
     return(
-        <p>Loading Content</p>
+        <DataFetch />
     )
 }
 
