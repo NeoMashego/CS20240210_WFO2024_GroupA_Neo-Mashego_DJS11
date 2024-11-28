82433ab2d71b258bfbf8b8574d99235f76cd20be
NeoMashego <mashegonoj@gmail.com>
2024-11-28 21:52:35 +0200

    spelling corrections

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom"
 import Home from './Pages/Home.jsx'
 import HomeDetail from "./Pages/HomeDetail.jsx"
-import Favourites from "./Pages/Favourites.jsx"
+import Favorites from "./Pages/Favourites.jsx"
 import './App.css'
 
 function App() {
@@ -10,11 +10,12 @@ function App() {
     <BrowserRouter>
       <nav>
         <Link to="/"> Home </Link>
-        <Link to="/favourites">Favourites</Link>
+        <Link to="/favorites">Favorites</Link>
       </nav>
       <Routes>
         <Route path="/" element={< Home/>} />
         <Route path="/:id" element={< HomeDetail/>} />
+        <Route path="/favorites" element={< Favorites />} />
       </Routes>
     </BrowserRouter>
   )
