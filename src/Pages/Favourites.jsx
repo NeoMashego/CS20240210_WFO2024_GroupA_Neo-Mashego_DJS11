96d9878e222d93245f19414278dee70f2f6e87b6
NeoMashego <mashegonoj@gmail.com>
2024-11-28 21:53:52 +0200

    set up for favorites display created

--- src/Pages/Favourites.jsx
+++ src/Pages/Favourites.jsx
@@ -1,7 +1,46 @@
 import { useEffect, useState } from "react";
 
-function Favourites(){
+const displayFavourites = () =>{
+    const [favorites, setFavorites] = useState([])  //set functionality
+    const [error, setError] = useState("")
 
+    useEffect( ()=>{
+        const storedFavorites = localStorage.getItem('Favorites:')
+
+        if(storedFavorites){
+            setFavorites(JSON.parse(storedFavorites))
+        }
+        
+    }, [])
+
+    //there are no favourites 
+    if(favorites.length === 0){
+        return <h1>User has no favorite episodes yet.</h1>
+    }
+
+    const display = <div className="favoritesBox">
+                                <h2>Your Favorite Episodes</h2>
+                                <ul>
+                                    {favorites.map((e, index)=>(
+                                        <div key={index} className="favoritesItem">
+                                            <h5>Episode {e.episode}: {e.title}</h5>
+                                                <p>{e.description}</p>
+                                                <audio controls>
+                                                    <source src={e.file} type="audio.mpeg" />
+                                                </audio>
+                                        </div>
+                                    ))}
+                                </ul>
+                            </div>
+
+    return(
+        {display}
+    )
+    
 }
 
-export default Favourites
+function Favorites(){
+    {displayFavourites}
+}
+
+export default Favorites
