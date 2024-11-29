d77df5c1dec8d521ee26a890151a946b4529f8a1
NeoMashego <mashegonoj@gmail.com>
2024-11-29 12:14:44 +0200

    set up favorites file with fetched data

--- src/Pages/Favourites.jsx
+++ src/Pages/Favourites.jsx
@@ -1,46 +1,61 @@
 import { useEffect, useState } from "react";
 
-const displayFavourites = () =>{
+const DisplayFavorites = () =>{
     const [favorites, setFavorites] = useState([])  //set functionality
     const [error, setError] = useState("")
+    const [loading, setLoading] = useState(true)
 
     useEffect( ()=>{
-        const storedFavorites = localStorage.getItem('Favorites:')
+        const storedFavorites = JSON.parse(localStorage.getItem('Favorites:'))
 
         if(storedFavorites){
-            setFavorites(JSON.parse(storedFavorites))
-        }
-        
+            try {
+                setFavorites(storedFavorites)
+            } catch(error){
+                setError("Error cannot parse favorites data")
+                setLoading(false)
+            }   } 
+            else {
+                setError("No favorites data was found in localStorage.")
+                setLoading(false)
+            }
     }, [])
+
+    if(loading){
+        return <div className="loadingDiv">
+                    <div className="loading"></div>
+                    <h2>Loading...</h2>
+                </div>
+    }
+
+    if(error){
+        return <h1>{error}</h1>
+    }
+    
 
     //there are no favourites 
     if(favorites.length === 0){
         return <h1>User has no favorite episodes yet.</h1>
     }
 
-    const display = <div className="favoritesBox">
-                                <h2>Your Favorite Episodes</h2>
-                                <ul>
-                                    {favorites.map((e, index)=>(
-                                        <div key={index} className="favoritesItem">
-                                            <h5>Episode {e.episode}: {e.title}</h5>
-                                                <p>{e.description}</p>
-                                                <audio controls>
-                                                    <source src={e.file} type="audio.mpeg" />
-                                                </audio>
-                                        </div>
-                                    ))}
-                                </ul>
-                            </div>
-
     return(
-        {display}
-    )
+            <div className="favoritesBox">
+                <h2>Your Favorite Episodes</h2>
+                    {favorites.map((e, index)=>(
+                        <div key={index} className="favoritesItem">
+                            <h5>Episode {e.episode}: {e.title}</h5>
+                            <p>{e.description}</p>
+                            <audio controls>
+                                <source src={e.file} type="audio.mpeg" />
+                            </audio>
+                        </div>
+                    ))}
+            </div>)
     
 }
 
 function Favorites(){
-    {displayFavourites}
+    return <DisplayFavorites />
 }
 
 export default Favorites
