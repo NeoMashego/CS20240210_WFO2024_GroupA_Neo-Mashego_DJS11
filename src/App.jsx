06f9f2cfe0c3bccb4058e35abd073ba54c5a0215
NeoMashego <mashegonoj@gmail.com>
2024-11-29 16:07:44 +0200

    place favorites to globally render

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom"
 import Home from './Pages/Home.jsx'
 import HomeDetail from "./Pages/HomeDetail.jsx"
@@ -7,6 +8,12 @@ import './App.css'
 
 function App() {
 
+      //mounts favorites (memory) content when rendered. global rendering
+      const [favorites, setFavorites] = useState( ()=>{
+        const savedFavorites = localStorage.getItem('Favorites')
+        return savedFavorites ? JSON.parse(savedFavorites) : []
+      })
+
   return (
     <BrowserRouter>
       <nav>
@@ -15,8 +22,8 @@ function App() {
       </nav>
       <Routes>
         <Route path="/" element={< Home/>} />
-        <Route path="/:id" element={< HomeDetail/>} />
-        <Route path="/favorites" element={< Favorites />} />
+        <Route path="/:id" element={< HomeDetail favorites={favorites} setFavorites={setFavorites} /> } />
+        <Route path="/favorites" element={< Favorites favorites={favorites} setFavorites={setFavorites} />} />
         <Route path="/genre/:id" element={< Genres />} />
       </Routes>
       {/*<AudioPlayer />*/}
