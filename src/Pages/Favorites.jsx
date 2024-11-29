028a43cb83499d1163fdf917f4094b2befbc7835
NeoMashego <mashegonoj@gmail.com>
2024-11-29 16:09:26 +0200

    set up Favourites file again

--- src/Pages/Favorites.jsx
+++ src/Pages/Favorites.jsx
@@ -1,63 +1,29 @@
 import { useEffect, useState } from "react";
 
-const DisplayFavorites = () =>{
-    const [favorites, setFavorites] = useState([])  //set functionality
-    const [error, setError] = useState("")
-    const [loading, setLoading] = useState(true)
-
-    useEffect( ()=>{
-        const storedFavorites = JSON.parse(localStorage.getItem('Favorites:'))
-
-        if(storedFavorites){
-            try {
-                setFavorites(storedFavorites)
-            } catch(error){
-                setError("Error cannot parse favorites data")
-                setLoading(false)
-            }   } 
-            else {
-                setError("No favorites data was found in localStorage.")
-                setLoading(false)
-            }
-    }, [])
-
-
-    //if statements for set conditions
-    if(loading){
-        return <div className="loadingDiv">
-                    <div className="loading"></div>
-                    <h2>Loading...</h2>
-                </div>
-    }
-
-    if(error){
-        return <h1>{error}</h1>
-    }
-    
-    //there are no favourites 
-    if(favorites.length === 0){
-        return <h1>User has no favorite episodes yet.</h1>
-    }
-
+const DisplayFavorites = ( { favorites }) =>{
 
     return(
-            <div className="favoritesBox">
+            <div className="favoritesPage">
                 <h2>Your Favorite Episodes</h2>
-                    {favorites.map((f)=>(
+                {favorites.length === 0 ? ( <p>No Favourites yet</p>) : (
+                    <div className="favouritesList">
+                        {favorites.map((f)=>(
                         <div key={f.title} className="favoritesItem">
                             <h5>Episode {f.episode}: {f.title}</h5>
                             <p>{f.description}</p>
                             <audio controls>
-                                <source src={f.file} type="audio.mpeg" />
+                                <source src={f.file} type="audio/mpeg" />
                             </audio>
                         </div>
                     ))}
+                    </div>
+                )}
             </div>)
     
 }
 
-function Favorites(){
-    return <DisplayFavorites />
+function Favorites({favorites}){
+    return <DisplayFavorites favorites={favorites} />
 }
 
 export default Favorites
